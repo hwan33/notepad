15646e89f9eba31bfde23febb7cf5356524cd97f
권용환 <hahahaa_a@naver.com>
2023-04-09 21:32:05 +0900

    accountFor 함수 추출하기

--- src/main/java/com/example/notepad/refactoring/ch01/RefactoringSample.java
+++ src/main/java/com/example/notepad/refactoring/ch01/RefactoringSample.java
@@ -1,89 +1,95 @@
 package com.example.notepad.refactoring.ch01;
 
+import com.example.notepad.refactoring.ch01.RefactoringSample.Invoices.Performances;
 import java.util.*;
 
 public class RefactoringSample {
-    private static class Play {
-        private String name;
-        private String type;
+  public static String statement(Invoices invoice, Map<String, Play> plays) throws Exception {
+    var totalAmount = 0;
+    var volumeCredits = 0;
+    var result = "청구 내역 고객명 : " + invoice.customerName + '\n';
 
-        public Play(String name, String type) {
-            this.name = name;
-            this.type = type;
-        }
+    for (var perf : invoice.performances) {
+      Play play = plays.get(perf.playId);
+      int thisAmount = amountFor(perf, play);
+
+      volumeCredits += Math.max(perf.audience - 30, 0);
+      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+
+      result += play.name + ": " + thisAmount + "원, " + perf.audience + "석\n";
+      totalAmount += thisAmount;
     }
 
-    private static class Invoices{
-        private String customerName;
-        private List<Performances> performances;
+    result += "총액: " + totalAmount + "원\n";
+    result += "적립 포인트: " + volumeCredits + "점\n";
+    return result;
+  }
 
-        private static class Performances{
-            private String playId;
-            private int audience;
+  private static int amountFor(Performances perf, Play play) throws Exception {
+    var thisAmount = 0;
 
-            public Performances(String playId, int audience) {
-                this.playId = playId;
-                this.audience = audience;
-            }
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
         }
-
-        public Invoices(String customerName, List<Performances> performances) {
-            this.customerName = customerName;
-            this.performances = performances;
+        break;
+      case "comedy":
+          thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
         }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Exception("알 수 없는 장르");
+    }
+    return thisAmount;
+  }
+
+  public static void main(String[] args) throws Exception {
+    List<Invoices.Performances> performances = new ArrayList<>();
+    performances.add(new Invoices.Performances("hamlet", 55));
+    performances.add(new Invoices.Performances("as-like", 35));
+    performances.add(new Invoices.Performances("othello", 40));
+
+    Invoices invoices = new Invoices("BigCo", performances);
+    Map<String, Play> plays = new HashMap<>();
+    plays.put("hamlet", new Play("Hamlet", "tragedy"));
+    plays.put("as-like", new Play("As You Like It", "comedy"));
+    plays.put("othello", new Play("Othello", "tragedy"));
+
+    System.out.println(statement(invoices, plays));
+  }
+
+  private static class Play {
+    private String name;
+    private String type;
+
+    public Play(String name, String type) {
+      this.name = name;
+      this.type = type;
+    }
+  }
+
+  static class Invoices {
+    private String customerName;
+    private List<Performances> performances;
+
+    public Invoices(String customerName, List<Performances> performances) {
+      this.customerName = customerName;
+      this.performances = performances;
     }
 
-    public static String statement(Invoices invoice, Map<String, Play> plays) throws Exception {
-        var totalAmount = 0;
-        var volumeCredits = 0;
-        var result = "청구 내역 고객명 : " + invoice.customerName + '\n';
+    static class Performances {
+      private String playId;
+      private int audience;
 
-        for(var perf : invoice.performances){
-            Play play = plays.get(perf.playId);
-            var thisAmount = 0;
-
-            switch(play.type){
-                case "tragedy":
-                    thisAmount = 40000;
-                    if(perf.audience > 30){
-                        thisAmount += 1000 * (perf.audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if(perf.audience > 20){
-                        thisAmount += 10000 + 500 * (perf.audience - 20);
-                    }
-                    thisAmount += 300 * perf.audience;
-                    break;
-                default:
-                    throw new Exception("알 수 없는 장르");
-            }
-
-            volumeCredits += Math.max(perf.audience - 30, 0);
-            if("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
-
-            result += play.name + ": " + thisAmount + "원, " + perf.audience + "석\n";
-            totalAmount += thisAmount;
-        }
-
-        result += "총액: " + totalAmount + "원\n";
-        result += "적립 포인트: " + volumeCredits + "점\n";
-        return result;
+      public Performances(String playId, int audience) {
+        this.playId = playId;
+        this.audience = audience;
+      }
     }
-
-    public static void main(String[] args) throws Exception {
-        List<Invoices.Performances> performances = new ArrayList<>();
-        performances.add(new Invoices.Performances("hamlet",55));
-        performances.add(new Invoices.Performances("as-like",35));
-        performances.add(new Invoices.Performances("othello",40));
-
-        Invoices invoices = new Invoices("BigCo", performances);
-        Map<String, Play> plays = new HashMap<>();
-        plays.put("hamlet",new Play("Hamlet", "tragedy"));
-        plays.put("as-like",new Play("As You Like It", "comedy"));
-        plays.put("othello",new Play("Othello", "tragedy"));
-
-        System.out.println(statement(invoices, plays));
-    }
+  }
 }
